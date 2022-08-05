0f4e4e4aedc8af85fbf265058661a656ad27f345
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-08-04 21:49:56 -0500

    added work component

---
+++ client/src/pages/Work/Work.js
@@ -0,0 +1,83 @@
+import React,{useState} from 'react'
+import Nav from '../../Components/Navbar/Nav'
+import './work.css'
+import Typewriter from 'typewriter-effect'
+import Carousel from '../../Components/Carousel';
+import { myWorkdata } from './workData';
+import Footer from '../../Components/Footer'
+
+function Work() {
+
+  return (
+    <div>
+      <Nav />
+      <div className='wrapper'>
+        <section className='hero-image'>
+          <div className='hero-section'>
+            <div className='djigo-photographer'>
+            </div>
+          </div>
+        </section>
+        <div className='hero-content-two'>
+          <div className='line'></div>
+        </div>
+
+        <div className='work-area'>
+          <div className='work-area-content'>
+            <h2 className='section-heading'><span className='span'>My</span>Work</h2>
+            <div className='typewrite'>
+              <Typewriter  onInit={(typewriter) =>{
+                typewriter.typeString('Please take your time and checkout my hard work').pauseFor(1000).deleteAll().start()
+                typewriter.typeString('I apreciate it').pauseFor(1000).start()
+              }}/>
+            </div>
+          </div>
+        </div>
+
+        {/* wrapper start */}
+        <div className="portfolio-items-wrapper ">
+          {/* one */}
+          {myWorkdata.map((item) => (
+          <a href={item.path} className='div-col'>
+            <div className="portfolio-item-wrapper ">
+              <div className="portfolio-img-background item"><img className='portfolio-img-background item' src={item.image} alt="" />
+              </div>
+              <div className="img-text-wrapper">
+                <div className="logo-wrapper">
+                  {item.techs.map((tech) => (
+                    <i className={tech}></i>
+                  ))}
+                </div>
+                <div className="subtitle">
+                  <h2>{item.title}</h2>
+                  <p>{item.text}</p>
+                </div>
+              </div>
+            </div> 
+          </a>
+          ))} 
+        </div>
+        <div className='grid-bottom-line'>
+          <div className='line'></div>
+        </div>
+        <div className='section-my-skills'>     
+          <h2 className='section-heading'><span className='span'>My</span> Skills</h2>
+        </div>
+        <div className='carousel-section'>
+        <Carousel /> 
+        </div>
+
+        <div className='grid-bottom-line'>
+          <div className='line'></div>
+        </div>
+        <div className='section-my-skills'>     
+          <h2 className='section-heading'><span className='span'>My</span> Reviews</h2>
+        </div>
+      </div> 
+
+      <Footer />   
+    </div>
+  )
+}
+
+export default Work
---
+++ client/src/pages/Work/workData.js
@@ -0,0 +1,143 @@
+export const myWorkdata = [
+  {
+    id:1,
+    title:"Weather Dashboard",
+    text:"This app is built using HTML, CSS, JS .An app that i've built on my own 2022, this app will give access to a daily and weekly wether status in your curent area",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js'],
+    image:"https://i.postimg.cc/C59tXy8H/weather.webp",
+    path:""
+  },
+  {
+    id:2,
+    title:"Pizza Hunt",
+    text:"This app is an MREN application. its a training project that will allow you to create your own pizza and checkout",
+    techs: ['fa-brands fa-react', 'fa-solid fa-database icon'],
+    image:"https://i.postimg.cc/253tk8xF/pizza.webp",
+    path:"https://pizza-hunt-hunt.herokuapp.com/"
+  },
+  {
+    id:3,
+    title:"Coffee Shop",
+    text:"This app is built using HTML, CSS, JS and SQL for backend database. an app that give you access to different types of coffee",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js', 'fa-solid fa-database icon'],
+    image:"https://i.postimg.cc/kMs09ZQc/coffee.jpg",
+    path:"https://coffee-from-the-world.herokuapp.com/"
+  },
+  {
+    id:4,
+    title:"Motor Bike Labs",
+    text:"This app is an MREN application. An app that i've built on my own 2022, this app will give access to many motorcycles and different brans, you can see infos of every motorcycle.",
+    techs: ['fa-brands fa-react', 'fa-solid fa-database icon'],
+    image:"https://i.postimg.cc/cHn5392v/motor.webp",
+    path:"https://motor-bike-labs.herokuapp.com/"
+  },
+  {
+    id:5,
+    title:"Track Your Budget",
+    text:"A PWA application that ive built, this app will give access to track your expenses, you also can download this app in your device.",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js', 'fa-solid fa-download icon'],
+    image:"https://i.postimg.cc/6pkSYNK9/budjet.jpg",
+    path:"https://my-own-budget.herokuapp.com/"
+  },
+  {
+    id:5,
+    title:"Taskinator",
+    text:"A PWA application that ive built, this app will give access to track your expenses, you also can download this app in your device.",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js', 'fa-solid fa-download icon'],
+    image:"https://i.postimg.cc/6pkSYNK9/budjet.jpg"
+  },
+  {
+    id:6,
+    title:"Post",
+    text:"This app is built using HTML, CSS, JS and SQL for database, An app that will give you access to add posts and comment on someone's post",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js', 'fa-solid fa-download, fa-solid fa-database icon'],
+    image:"https://i.postimg.cc/BnnYpQR9/post.jpg",
+    path:"https://safe-meadow-38060.herokuapp.com/"
+  },
+  {
+    id:7,
+    title:"Work day schedule",
+    text:"This app is built using HTML, CSS, JS. an app that doest have a database, but im using localStorage to save my data",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js icon'],
+    image:"https://i.postimg.cc/25nttXjn/schedule.webp",
+    path:"https://jmaouchi.github.io/Work-Day-Scheduler/"
+  },
+  {
+    id:8,
+    title:"note taker",
+    text:"This app is built using HTML, CSS, JS. An app that allow you to save your notes for later use case, and delete them whenever you want",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js'],
+    image:"https://i.postimg.cc/C5dT9HQY/note.jpg",
+    path:'https://whispering-crag-45136.herokuapp.com/'
+  },
+  {
+    id:9,
+    title:"Add Thoughts",
+    text:"This app is built using HTML, CSS, JS and SQL for database",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js, fa-solid fa-database icon'],
+    image:"https://i.postimg.cc/2SQJcKSh/thoughts.webp",
+    path:"https://young-mesa-86124.herokuapp.com/"
+  },
+  {
+    id:9,
+    title:"Zoo Keepr",
+    text:"This app is built using HTML, CSS, JS and SQL for database.",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js, fa-solid fa-database icon'],
+    image:"https://i.postimg.cc/prPVNgW9/zoo.jpg",
+    path:"https://young-mesa-86124.herokuapp.com/"
+  },
+  {
+    id:10,
+    title:"Task Master",
+    text:"This app is built using HTML, CSS, JS. i created this application to train myself on using JQuery (javaScript library)",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js'],
+    image:"https://i.postimg.cc/CLkS0JKL/task-master.jpg"
+  },
+  {
+    id:11,
+    title:"Drive Now",
+    text:"This is a little game that I've created on my own, the game is not done yet!",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js'],
+    image:"https://i.postimg.cc/vmSpf6mN/drive.jpg"
+  },
+  {
+    id:11,
+    title:"Book Search",
+    text:"This is a book search site that will allow you serch some books by name, and save them for later",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js'],
+    image:"https://i.postimg.cc/qRR8WQQx/book.jpg",
+    path:"https://whispering-earth-59194.herokuapp.com/"
+  },
+  {
+    id:12,
+    title:"Quiz",
+    text:"This a little quiz that i made as a challenge for my school, fell free to checkout it out!",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js'],
+    image:"https://i.postimg.cc/d0D2x45m/question.jpg",
+    path:"https://jmaouchi.github.io/Question-and-Answer-Game-/quiz.html"
+  },
+  {
+    id:12,
+    title:"Add Task",
+    text:"Task list that i've made before starting my coding school.",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3', 'fa-brands fa-js'],
+    image:"https://i.postimg.cc/Zq2FczPh/task.jpg",
+    path:"https://jmaouchi.github.io/Task-List-/"
+  },
+  {
+    id:1,
+    title:"Run Buddy",
+    text:"This app is built using HTML and CSS while learning flexbox",
+    techs: ['fa-brands fa-html5', 'fa-brands fa-css3'],
+    image:"https://i.postimg.cc/fyj4xK9Y/run.webp",
+    path:""
+  },
+  {
+    id:13,
+    title:"GitHub",
+    text:"I still have some server side apps that i made, please checkout my github to be able to clone them in your local machine",
+    techs: ['fa-brands fa-github'],
+    image:"https://i.postimg.cc/X7JWWNgc/git.webp",
+    path:"https://whispering-earth-59194.herokuapp.com/"
+  },
+]
