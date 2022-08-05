56fb0f5b67fa9ee51760a30203e5dd0a41f4fc1e
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-08-04 21:49:12 -0500

    added main page component

---
+++ client/src/pages/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../../Components/Navbar/Nav"
+import Me from '../../Components/Me/index'
+
+function Main() {
+  return (
+    <div>
+      <Navbar />
+      <Me />
+    </div>
+  )
+}
+
+export default Main
