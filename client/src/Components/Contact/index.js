8601d95688b405e249c876815a78c3b455c7b91c
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-08-06 23:28:15 -0500

    media query

--- client/src/Components/Contact/index.js
+++ client/src/Components/Contact/index.js
@@ -27,69 +27,70 @@ function ContactPage() {
   return (
     <>
     <Nav />
-      {
-        <section className='tile is-ancestor  has-text-centered is-flex-wrap-wrap contact-tile contact'>
-        <div className='tile is-parent has-text-centered mx-6 my-6 px-4 is-7 parent-contact '>
-          <div className='tile is-child box column contact-box border-tile child-contact'>
-            <h5 className='title is-3 my-4 mt-6 contact-title'>Contact Me</h5>
-            {/*Add contact form*/}
-            <form className='field contact-field my-6' ref={form} onSubmit={sendEmail}>
-              <h2 className='is-4 my-6 contact-sub'>Want to setup some time to talk?</h2>
-              <div className='level  level-field '>
-                <div className='level-item is-flex-wrap-wrap mt-2'>
-                  <div className='level-item mx-1'>
-                    <input
-                      className='input level-item level-input input-border '
-                      placeholder='Full Name'
-                      id='first'
-                      name='name'
-                      required
-                    ></input>
+      { 
+        <div className='main-contact-page'>
+          <section className='tile is-ancestor  has-text-centered is-flex-wrap-wrap contact-tile contact'>
+          <div className='tile is-parent has-text-centered mx-6 my-6 px-4 is-7 parent-contact '>
+            <div className='tile is-child box column contact-box border-tile child-contact'>
+              <h5 className='title my-4 mt-6 contact-title'>Contact Me</h5>
+              {/*Add contact form*/}
+              <form className='field contact-field my-6' ref={form} onSubmit={sendEmail}>
+                <h2 className='my-4 contact-sub contact-infos-paragraph'>Want to setup some time to talk?</h2>
+                <div className='level  level-field '>
+                  <div className='level-item is-flex-wrap-wrap mt-2'>
+                    <div className='level-item mx-1'>
+                      <input
+                        className='input level-item level-input input-border '
+                        placeholder='Full Name'
+                        id='first'
+                        name='name'
+                        required
+                      ></input>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className='level level-field '>
-                <div className='level-item is-flex-wrap-wrap'>
-                  <div className='level-item mx-1 '>
-                    <input
-                      className='input level-item level-input input-border'
-                      placeholder='Email'
-                      id='email'
-                      name='email'
-                      required
-                    ></input>
-                  </div>
+                <div className='level level-field '>
                   <div className='level-item is-flex-wrap-wrap'>
-                    <input
-                      className='input level-item  level-input input-border'
-                      placeholder='Phone Number'
-                      id='phone'
-                      name='phone'
-                      required
-                    ></input>
+                    <div className='level-item mx-1 '>
+                      <input
+                        className='input level-item level-input input-border'
+                        placeholder='Email'
+                        id='email'
+                        name='email'
+                        required
+                      ></input>
+                    </div>
+                    <div className='level-item is-flex-wrap-wrap'>
+                      <input
+                        className='input level-item  level-input input-border'
+                        placeholder='Phone Number'
+                        id='phone'
+                        name='phone'
+                        required
+                      ></input>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className='field my-4  px-6 here'>
-                <div className='control'>
-                  <textarea
-                    className='textarea is-small level-field input-border'
-                    id='text'
-                    placeholder="says"
-                    name='message'
-                    required
-                  ></textarea>
+                <div className='field my-4  px-6 here'>
+                  <div className='control'>
+                    <textarea
+                      className='textarea is-small level-field input-border'
+                      id='text'
+                      placeholder="says"
+                      name='message'
+                      required
+                    ></textarea>
+                  </div>
                 </div>
-              </div>
-              <button className='button input-border' id=''>
-                Contact
-              </button>
-            </form>
+                <button className='button input-border' id=''>
+                  Contact
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      </section>
-        
+        </section>       
+      </div>
       }
       <div>
         {result ? <Result result={result} showResult={showResult}/> : null}
