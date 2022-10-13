c607336ccef1bd7dc52fd2821d9f36bd24be3bf0
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-10-13 16:31:12 -0500

    footer update

--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react'
+import { FaLinkedin } from "react-icons/fa";
+import {FaLocationArrow} from "react-icons/fa"
+import {FaInstagram} from "react-icons/fa"
 import './footer.css'
 
 function Footer() {
@@ -6,11 +9,40 @@ function Footer() {
   return (
     <div>
       <footer className='footer'> 
-        <p className='footer-author'>
-          &copy; 2022 | Built with <span></span> 💙 by Jugurta Maouchi
-        </p> 
+        <div className='contact-side'>
+          <div className='right-side'>
+            <span className='contact-email'>Email :</span> 
+            <p>
+            djigo.maouchi@yahoo.com
+            </p>
+          </div>
+          <div className='left-side'>
+            <span className='contact-based'>Based In : </span>
+            <FaLocationArrow></FaLocationArrow>
+            <p>
+              San Antonio 
+            </p>
+          </div>
+          <div>
+            Linkdin 
+            <p>< FaLinkedin ></FaLinkedin></p>
+          </div>
+          <div>
+            Instagram
+            <p>< FaInstagram></FaInstagram></p>
+          </div>
+          <div>
+            <p>< FaLinkedin ></FaLinkedin></p>
+          </div>
+        </div>
       </footer>
+      {/* <div>
+            <p className='footer-author'>
+              &copy; 2022 | Built with <span></span> 💙 by Jugurta Maouchi
+            </p> 
+          </div> */}
     </div>
+    
   )
 }
 
