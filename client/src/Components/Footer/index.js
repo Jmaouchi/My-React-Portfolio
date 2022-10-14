a2ca32112393a13d7382e73d74b42dd85454a6e4
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-10-14 12:12:21 -0500

    Footer mobile responsiveness

--- client/src/Components/Footer/index.js
+++ client/src/Components/Footer/index.js
@@ -11,19 +11,25 @@ function Footer() {
     <div>
       <footer className='footer'> 
           <a href='https://www.linkedin.com/in/jugurta-maouchi-840b08227/'>
-          <div className='footer-context'>
-            <FaRegEnvelope className='here'></FaRegEnvelope>
-          </div>
-            </a>
-          <div className='footer-context'>
-            <FaLocationArrow className='here'></FaLocationArrow>
-          </div>
+            <div className='footer-context'>
+              <FaRegEnvelope className='here'></FaRegEnvelope>
+            </div>
+          </a>
+          <a href='https://www.linkedin.com/in/jugurta-maouchi-840b08227/'>
+            <div className='footer-context'>
+              <FaLocationArrow className='here'></FaLocationArrow>
+            </div>
+          </a>
+          <a href='https://www.linkedin.com/in/jugurta-maouchi-840b08227/'>
           <div className='footer-context'>
             < FaInstagram className='here'></FaInstagram>
           </div>
+          </a>
+          <a href='https://www.linkedin.com/in/jugurta-maouchi-840b08227/'>
           <div className='footer-context'>
             < FaLinkedin className='here'></FaLinkedin>
           </div>
+          </a>
       </footer>
     </div>
     
